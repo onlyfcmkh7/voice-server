8abd424615c00724abd47d2c90b6f9b4aa1d0035
onlyfcmkh7 <onlyfcmkh7@gmail.com>
2026-04-24 17:02:07 +0300

    Update telegramClient.js

--- telegramClient.js
+++ telegramClient.js
@@ -5,7 +5,7 @@ import input from "input";
 const apiId = 32996008;
 const apiHash = "ec81a5a75b0180b6e514ce2afaf3278b";
 
-// тимчасово порожня сесія, щоб отримати новий SESSION STRING
+const stringSession = new StringSession("1AgAOMTQ5LjE1NC4xNjcuNDEBu05AFOouIgIJ1vpUAq8VSa4GBV33gK/LuGB7yrvnUKLhwiZqHGhYMBBW4cYFFlgSYhbottCa3H8AzBNYS26FhoziUUxZyELS+igR7Q3EjQy43rfbzH6BO4xk6O85lwoAags/Navj3a8jmcZ4vfRPK9YPPmFfqPv6PjzF7CF036Ce8k/OFCWcZvFe+PbeJz4Zez74LnZNZTgokocO8j7/+HuLsqxpHNzx1AYakazVCDwbi50Aj9zcmWCMSXbQF4E55g1yvNkJ9q32lQiKyvL9QtkD3kFa81noIGRcshnOSZAxo3IkDhMAZRx5NKAU89PFt9XwmE+fcwVG+tkqMWaSLlU=");
 const stringSession = new StringSession("");
 
 export const client = new TelegramClient(stringSession, apiId, apiHash, {
