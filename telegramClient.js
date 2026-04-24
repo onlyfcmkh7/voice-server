67361a1af3bb797b135b8caac9a7ba2b418142f3
onlyfcmkh7 <onlyfcmkh7@gmail.com>
2026-04-24 16:55:57 +0300

    Update telegramClient.js

--- telegramClient.js
+++ telegramClient.js
@@ -5,7 +5,8 @@ import input from "input";
 const apiId = 32996008;
 const apiHash = "ec81a5a75b0180b6e514ce2afaf3278b";
 
-const stringSession = new StringSession("1AgAOMTQ5LjE1NC4xNjcuNDEBu41XdoFbUZO2MWH2PjcBqwuYFXT38D6KDiqJnv7demE+6AJ+iuP0WZ/OZEgprydp/kDTAtLIyvAby2gq7aRvSsJA/UMUq+LCCNoHCqfDUjQhb2UYFUI4SHdq2gZDVmJcXJxK2CtpFq9NJ2GVIL1j56u8mfsTukVom5liQczGzCOlKy/3CuCl/j+mRPGtvGNHQoWEjyyVs/jY8AZhixv6guarLztqLok1mXaXCCFF7vTBms5h8LG8h3/h9+tq5IjtE4N60D2OsDscH/R4zjFdk6Lfrc/KSPobLUrL03/LYF6qZUeCGjiSUhS+Qo+eo7fcOy2JAjpF8S1epxvvXNoSLbk=");
+// тимчасово порожня сесія, щоб отримати новий SESSION STRING
+const stringSession = new StringSession("");
 
 export const client = new TelegramClient(stringSession, apiId, apiHash, {
   connectionRetries: 5,
@@ -24,7 +25,9 @@ export async function startTelegram() {
   });
 
   telegramStarted = true;
+
   console.log("Telegram підключений");
+  console.log("SESSION STRING:", client.session.save());
 }
 
 export async function getUnreadTelegramMessages(limitPerDialog = 10) {
