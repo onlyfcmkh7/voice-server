8fa4152766f848209ea963158b88134e65fc36b0
onlyfcmkh7 <onlyfcmkh7@gmail.com>
2026-04-27 18:02:03 +0300

    Update weatherRoutes.js

--- weatherRoutes.js
+++ weatherRoutes.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const router = express.Router();
 
+router.get("/weather", async (req, res) => {
+    try {
+        const city = req.query.city || "Kyiv";
+
+        // 🔥 поки заглушка
+        const text = `Погода в ${city}: +20°C, сонячно ☀️`;
+
+        res.json({
+            text
+        });
+
+    } catch (e) {
+        res.status(500).json({
+            text: "Помилка погоди"
+        });
+    }
+});
+
+module.exports = router;
