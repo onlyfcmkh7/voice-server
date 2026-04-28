209a0e58c414819ed9a7c7f5629a260e55a4e9be
onlyfcmkh7 <onlyfcmkh7@gmail.com>
2026-04-28 20:04:01 +0300

    Update gmailClient.js

--- gmailClient.js
+++ gmailClient.js
@@ -1,9 +1,18 @@
+// gmailClient.js
+
 export async function getWorkUaEmails() {
   return [
     {
-      subject: "Тест Work.ua",
+      resume: "Android / AI Developer",
+      subject: "Work.ua",
       from: "no-reply@work.ua",
-      snippet: "Ваше резюме Android / AI Developer переглянули"
+      snippet: "Ваше резюме Android / AI Developer переглянули 1 раз"
+    },
+    {
+      resume: "Менеджер HoReCa",
+      subject: "Work.ua",
+      from: "no-reply@work.ua",
+      snippet: "Ваше резюме Менеджер HoReCa переглянули 4 рази"
     }
   ];
 }
