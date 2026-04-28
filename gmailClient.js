6562cbcc56acd6285184543ee6f8b9c224ebc72d
onlyfcmkh7 <onlyfcmkh7@gmail.com>
2026-04-28 20:00:39 +0300

    Create gmailClient.js

---
+++ gmailClient.js
@@ -0,0 +1,9 @@
+export async function getWorkUaEmails() {
+  return [
+    {
+      subject: "Тест Work.ua",
+      from: "no-reply@work.ua",
+      snippet: "Ваше резюме Android / AI Developer переглянули"
+    }
+  ];
+}
