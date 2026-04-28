ebff9ed107d430961779985ee05abc3d78bb98c2
onlyfcmkh7 <onlyfcmkh7@gmail.com>
2026-04-28 19:56:01 +0300

    Update workuaRoutes.js

--- workuaRoutes.js
+++ workuaRoutes.js
@@ -1,1 +1,11 @@
+import express from "express";
 
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  res.json({
+    text: "Work.ua моніторинг підключено. Нових оновлень поки не перевіряю."
+  });
+});
+
+export default router;
